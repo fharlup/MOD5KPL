01a810e177fc34ef3a73f273e68db0190333bff8
fharlup <35958282+fharlup@users.noreply.github.com>
2024-03-18 07:42:28 +0700

    “menambahkan class SimpleDataBase

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Penjumlahan<T> where T : struct
+{
+    public T Satu { get; private set; }
+    public T Dua { get; private set; }
+    public T Tiga { get; private set; }
+    int hasil;
+
+    public void JumlahTigaAngka(T satu, T dua, T tiga)
+    {
+        this.Satu = satu;
+        this.Dua = dua;
+        this.Tiga = tiga;
+        dynamic satua = satu;
+        dynamic duaa = dua;
+        dynamic tigaa = tiga;
+        dynamic hasil = satua + duaa + tigaa;
+        Console.WriteLine(hasil);
+
+
+    }
+   
+}
+public class SimpleDataBase<T> 
+
+{
+
+    List<DateTime> InputDate;
+    List<T> StoreData;
+    public  SimpleDataBase()
+    {
+        this.StoreData = new List<T>();
+        InputDate = new List<DateTime>();
+    }
+    public void addNewData (T NewData)
+    {
+        StoreData.Add(NewData);
+        InputDate.Add(DateTime.Now);
+    }
+    public void Printdata()
+    {
+        for(int i=0; i<StoreData.Count; i++)
+        {
+            Console.WriteLine($"Data {i+1} berisi: {StoreData.ElementAt(i)}, yang disimpan pada waktu {InputDate.ElementAt(i)}");
+        }
+    }
+}
+ public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Penjumlahan<float> penjumlah = new Penjumlahan<float>();
+            penjumlah.JumlahTigaAngka(1, 2, 3);
+            SimpleDataBase <string>data1=new SimpleDataBase<string>();
+        SimpleDataBase<string> data2 = new SimpleDataBase<string>();
+        data1.addNewData("12");
+        data1.addNewData("34");
+        data1.addNewData("34");
+        data1.Printdata();
+        
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
