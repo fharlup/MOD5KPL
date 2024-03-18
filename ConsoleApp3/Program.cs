87ffcce6c1a466871c4603bb6d3fa9134690e23f
fharlup <35958282+fharlup@users.noreply.github.com>
2024-03-18 07:04:23 +0700

    .

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Penjumlahan<T> where T : struct
+{
+    public T Satu { get; private set; }
+    public T Dua { get; private set; }
+    public T Tiga { get; private set; }
+    int hasil;
+
+    public void JumlahTigaAngka(T satu, T dua, T tiga)
+    {
+        this.Satu = satu;
+        this.Dua = dua;
+        this.Tiga = tiga;
+        dynamic satua = satu;
+        dynamic duaa = dua;
+        dynamic tigaa = tiga;
+        dynamic hasil = satua + duaa + tigaa;
+        Console.WriteLine(hasil);
+
+
+    }
+   
+}
+ public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Penjumlahan<float> penjumlah = new Penjumlahan<float>();
+            penjumlah.JumlahTigaAngka(1, 2, 3);
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
